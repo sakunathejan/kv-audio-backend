b1cae0974a285f588475e4a4fbcde37cc12f4216
Sakuna Thejan <sakuthejan200515@gmail.com>
2025-01-06 16:46:46 +0530

    Initialized the backend repository and set up the basic project structure. Created the authentication module to handle user login, registration, and token generation. Configured the database connections and included initial setup files for smooth integration. Also added test cases to ensure the authentication logic works as expected.

---
+++ index.js
@@ -0,0 +1,48 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import userRouter from "./routes/userRouter.js";
+import productRouter from "./routes/productRouter.js";
+import jwt from "jsonwebtoken";
+
+
+
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  let token = req.header("Authorization")
+
+  if (token != null) {
+    token = token.replace("Bearer ", ""); 
+    jwt.verify(token, "kv-secret-89!", (err, decoded) => {
+
+      if(!err){
+        req.user = decoded;
+      }
+    });
+  }
+  next()
+});
+
+let mongoUrl = "mongodb+srv://admin:admin123@cluster0.nlhhk.mongodb.net/prods?retryWrites=true&w=majority&appName=Cluster0"
+
+mongoose.connect(mongoUrl)
+
+let connection = mongoose.connection
+
+connection.once("open", () => {
+  console.log("MongoDB connection established successfully");
+});
+
+
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
