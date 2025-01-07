a26267377f1495b1294d289c6e6fd4788148ff88
Sakuna Thejan <sakuthejan200515@gmail.com>
2025-01-07 11:10:19 +0530

    Loaded the .env file to index.js file and connected the .env file to mongoUrl ✅

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ import mongoose from "mongoose";
 import userRouter from "./routes/userRouter.js";
 import productRouter from "./routes/productRouter.js";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
-
-
-
+dotenv.config();
 const app = express();
 
 app.use(bodyParser.json());
@@ -27,7 +26,7 @@ app.use((req, res, next) => {
   next()
 });
 
-let mongoUrl = "mongodb+srv://admin:admin123@cluster0.nlhhk.mongodb.net/prods?retryWrites=true&w=majority&appName=Cluster0"
+let mongoUrl = process.env.MONGO_URL
 
 mongoose.connect(mongoUrl)
 
